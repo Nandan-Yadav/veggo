69d13f105562aaa1d63e248ede95b5d374aaeb77
Nandan-Yadav <nandanyadav.putti@gmail.com>
2024-12-13 22:15:36 +0530

    Add custom theme for widgets (Text, AppBar, Checkbox, Chip, ElevatedButton, OutlinedButton, TextField)

---
+++ lib/utilities/themes/custom_theme/checkbox_theme.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class CCheckboxTheme {
+  CCheckboxTheme._();
+
+  static CheckboxThemeData lightCheckboxTheme = CheckboxThemeData(
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+    checkColor: WidgetStateProperty.resolveWith((states) {
+      if (states.contains(WidgetState.selected)) {
+        return Colors.white;
+      } else {
+        return Colors.black;
+      }
+    }),
+    fillColor: WidgetStateProperty.resolveWith((states) {
+      if (states.contains(WidgetState.selected)) {
+        return const Color(0xFF388E3C);
+      } else {
+        return Colors.transparent;
+      }
+    }),
+  );
+
+  static CheckboxThemeData darkCheckboxTheme = CheckboxThemeData(
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+    checkColor: WidgetStateProperty.resolveWith((states) {
+      if (states.contains(WidgetState.selected)) {
+        return Colors.white;
+      } else {
+        return Colors.black;
+      }
+    }),
+    fillColor: WidgetStateProperty.resolveWith((states) {
+      if (states.contains(WidgetState.selected)) {
+        return const Color(0xFF388E3C);
+      } else {
+        return Colors.transparent;
+      }
+    }),
+  );
+//
+}
---
+++ lib/utilities/themes/theme.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:veggo/utilities/themes/custom_theme/appbar_theme.dart';
+import 'package:veggo/utilities/themes/custom_theme/checkbox_theme.dart';
+import 'package:veggo/utilities/themes/custom_theme/chip_theme.dart';
+import 'package:veggo/utilities/themes/custom_theme/elevatedbutton_theme.dart';
+import 'package:veggo/utilities/themes/custom_theme/outlinedbutton_theme.dart';
+import 'package:veggo/utilities/themes/custom_theme/textfield_theme.dart';
+import 'custom_theme/text_theme.dart';
+
+class AppTheme {
+  AppTheme._();
+
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.light,
+    primaryColor: const Color(0xFF388E3C),
+    scaffoldBackgroundColor: Colors.white,
+    textTheme: CTextTheme.lightTextTheme,
+    appBarTheme: CAppBarTheme.lightAppBarTheme,
+    checkboxTheme: CCheckboxTheme.lightCheckboxTheme,
+    chipTheme: CChipTheme.lightChipTheme,
+    elevatedButtonTheme: CElevatedButtonTheme.lightElevatedButtonTheme,
+    outlinedButtonTheme: COutlinedButtonTheme.lightOutlinedButtonTheme,
+    inputDecorationTheme: CTextFormFieldTheme.lightInputDecorationTheme,
+  );
+
+  static ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.dark,
+    primaryColor: const Color(0xFF388E3C),
+    scaffoldBackgroundColor: Colors.black,
+    textTheme: CTextTheme.darkTextTheme,
+    appBarTheme: CAppBarTheme.darkAppBarTheme,
+    checkboxTheme: CCheckboxTheme.darkCheckboxTheme,
+    chipTheme: CChipTheme.darkChipTheme,
+    elevatedButtonTheme: CElevatedButtonTheme.darktElevatedButtonTheme,
+    outlinedButtonTheme: COutlinedButtonTheme.darkOutlinedButtonTheme,
+    inputDecorationTheme: CTextFormFieldTheme.darkInputDecorationTheme,
+  );
+}
