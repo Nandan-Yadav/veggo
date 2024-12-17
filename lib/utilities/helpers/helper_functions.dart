9683f8bfb27b98d6127eb2d063293f569e79d729
Nandan-Yadav <nandanyadav.putti@gmail.com>
2024-12-17 20:00:39 +0530

    feat: add helper_functions.dart with scaffold color utility
    - Created a reusable utility class  for app-wide helper methods
    - Added a static method  to fetch the scaffold background color dynamically based on the current theme
    - Simplifies theme-based UI customizations by centralizing common helper logic

---
+++ lib/utilities/helpers/helper_functions.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class HelperFunctions {
+  static getScaffoldColor(BuildContext context) {
+    return Theme.of(context).scaffoldBackgroundColor;
+  }
+
+  
+}
